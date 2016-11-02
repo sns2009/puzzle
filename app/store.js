eef3287e4f45b6823e3c59f40d6e4ca5ca1c1aae
sns2009 <sns.nazar@gmail.com>
2016-11-02 18:30:54 +0300

    space indent

---
+++ app/store.js
@@ -0,0 +1,11 @@
+import { applyMiddleware, createStore } from 'redux';
+import createLogger from 'redux-logger';
+import reducer from './reducers';
+
+const logger = createLogger();
+const store = createStore(
+  reducer,
+  applyMiddleware(logger)
+  );
+  
+export default store;
